67e289421af89b730bf8f43e877a0fbae9051da8
Pilich Alexandr <pilich.sasha@gmail.com>
2024-01-02 22:06:06 +0300

    FInal-a

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+const navMenu=document.getElementById('nav-menu'),
+navToggle=document.getElementById('nav-toggle'),
+navClose=document.getElementById('nav-close')
+
+if(navToggle)
+{
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+if(navClose)
+{
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+const navLink=document.querySelectorAll('nav__link')
+
+const linkAction=()=>
+{
+    const navMenu=document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=> n.addEventListener('click',linkAction)) 
+
+const swiperWatches = new Swiper('.home__swiper', {
+
+    loop: true,
+    spaceBetween:32,
+    grabCursor:true,
+    effect:'creative',
+    creativeEffect:
+    {
+      prev:{
+        translate:[-100,0,-500],
+        rotate:[0,0,15],
+        opacity:0
+      },
+      next:{
+        translate:[100,0,-500],
+        rotate:[0,0,-15],
+        opacity:0
+      },
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+
+    },
+    
+    
+ 
+  })
+
